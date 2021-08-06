e8aed36e2177a01f9c646d2a45a139b7be251618
Daniel McKnight <daniel@neongecko.com>
2021-08-06 16:46:31 -0700

    Refactor skill to simplify logic Make API skills configurable Handle returned icon path in idle.qml

--- __init__.py
+++ __init__.py
@@ -1,16 +1,13 @@
 import datetime
-import json
-import importlib.util
 import os
-import sys
 import time
 import requests
-from os import path
-from pathlib import Path
-from PIL import Image 
 
-from mycroft.messagebus.message import Message
-from mycroft.skills.core import MycroftSkill, resting_screen_handler, intent_file_handler
+from os import path
+from mycroft_bus_client import Message
+from neon_utils.log_utils import LOG
+
+from mycroft.skills.core import resting_screen_handler, intent_file_handler, MycroftSkill
 from mycroft.skills.skill_loader import load_skill_module
 from mycroft.skills.skill_manager import SkillManager
 from mycroft.skills.api import SkillApi
@@ -25,8 +22,17 @@ class OVOSHomescreen(MycroftSkill):
         self.notifications_storage_model = []
         self.def_wallpaper_folder = path.dirname(__file__) + '/ui/wallpapers/'
         self.loc_wallpaper_folder = self.file_system.path + '/wallpapers/'
-        self.selected_wallpaper = self.settings.get("wallpaper", "default.jpg")
+        self.selected_wallpaper = self.settings.get("wallpaper") or "default.jpg"
         self.wallpaper_collection = []
+        self.rtlMode = 1 if self.config_core.get("rtl", False) else 0
+
+        # Populate skill IDs to use for data sources
+        self.weather_skill = self.settings.get("weather_skill") or "skill-weather.openvoiceos"
+        self.datetime_skill = self.settings.get("datetime_skill") or "skill-date-time.mycroftai"
+        self.skill_info_skill = self.settings.get("examples_skill") or "ovos-skills-info.openvoiceos"
+        self.weather_api = None
+        self.datetime_api = None
+        self.skill_info_api = None
 
     def initialize(self):
         now = datetime.datetime.now()
@@ -36,7 +42,7 @@ class OVOSHomescreen(MycroftSkill):
         self.schedule_repeating_event(self.update_dt, callback_time, 10)
         self.skill_manager = SkillManager(self.bus)
 
-        # Handler Registeration For Notifications
+        # Handler Registration For Notifications
         self.add_event("homescreen.notification.set",
                        self.handle_display_notification)
         self.add_event("homescreen.wallpaper.set",
@@ -51,58 +57,23 @@ class OVOSHomescreen(MycroftSkill):
                                   self.handle_clear_notification_storage)
         self.gui.register_handler("homescreen.notification.storage.item.rm",
                                   self.handle_clear_notification_storage_item)
+
         self.add_event("mycroft.ready", self.handle_mycroft_ready)
         
         if not self.file_system.exists("wallpapers"):
             os.mkdir(path.join(self.file_system.path, "wallpapers"))
         
         self.collect_wallpapers()
+        self._load_skill_apis()
 
-        # Import Date Time Skill As Date Time Provider
-        # TODO - replace with Skills API call in 21.02
-        root_dir = self.root_dir.rsplit("/", 1)[0]
-        try:
-            time_date_path = str(root_dir) + "/skill-date-time.mycroftai/__init__.py"
-            time_date_id = "datetimeskill"
-            datetimeskill = load_skill_module(time_date_path, time_date_id)
-            from datetimeskill import TimeSkill
-
-            self.dt_skill = TimeSkill()
-        except:
-            self.log.error("Failed To Import DateTime Skill")
-
-        try:
-            self.weather_api = SkillApi.get('skill-weather.openvoiceos')
-        except:
-            self.log.error("Failed To Import Weather Skill")
-
-        self.schedule_repeating_event(self.update_weather, callback_time, 900)
-
-        try:
-            self.skill_info_api = SkillApi.get('ovos-skills-info.openvoiceos')
-        except:
-            self.log.error("Failed To Import OVOS Info Skill")
-
-        try:
-            is_rtl = self.config_core.get("rtl", False)
-            if is_rtl:
-                self.rtlMode = 1
-            else:
-                self.rtlMode = 0
-        except:
-            self.log.debug("RTL config not set")
+        self.bus.emit(Message("mycroft.device.show.idle"))
 
     #####################################################################
-    # Homescreen Registeration & Handling
+    # Homescreen Registration & Handling
 
     @resting_screen_handler("OVOSHomescreen")
     def handle_idle(self, _):
-        self.log.debug('Activating Time/Date resting page')
-        self.gui['time_string'] = self.dt_skill.get_display_current_time()
-        self.gui['date_string'] = self.dt_skill.get_display_date()
-        self.gui['weekday_string'] = self.dt_skill.get_weekday()
-        self.gui['month_string'] = self.dt_skill.get_month_date()
-        self.gui['year_string'] = self.dt_skill.get_year()
+        LOG.debug('Activating Time/Date resting page')
         self.gui['wallpaper_path'] = self.check_wallpaper_path(self.selected_wallpaper)
         self.gui['selected_wallpaper'] = self.selected_wallpaper
         self.gui['notification'] = {}
@@ -110,50 +81,55 @@ class OVOSHomescreen(MycroftSkill):
             "storedmodel": self.notifications_storage_model,
             "count": len(self.notifications_storage_model),
         }
+        self._load_skill_apis()
+        self.update_dt()
         self.update_weather()
-        self.gui['skill_examples'] = {"examples": self.skill_info_api.skill_info_examples()}
+        self.update_examples()
+
         self.gui['rtl_mode'] = self.rtlMode
         self.gui.show_page("idle.qml")
 
-    def handle_idle_update_time(self):
-        self.gui["time_string"] = self.dt_skill.get_display_current_time()
-        self.gui["date_string"] = self.dt_skill.get_display_date()
-        self.gui["weekday_string"] = self.dt_skill.get_weekday()
-        self.gui["month_string"] = self.dt_skill.get_month_date()
-        self.gui["year_string"] = self.dt_skill.get_year()
+    def update_examples(self):
+        """
+        Loads or updates skill examples via the skill_info_api.
+        """
+        if not self.skill_info_api:
+            LOG.warning(f"Requested update before API's loaded!")
+            self._load_skill_apis()
+        try:
+            self.gui['skill_examples'] = {"examples": self.skill_info_api.skill_info_examples()}
+        except Exception as e:
+            LOG.error(e)
 
     def update_dt(self):
-        self.gui["time_string"] = self.dt_skill.get_display_current_time()
-        self.gui["date_string"] = self.dt_skill.get_display_date()
-        self.gui["weekday_string"] = self.dt_skill.get_weekday()
-        self.gui["month_string"] = self.dt_skill.get_month_date()
-        self.gui["year_string"] = self.dt_skill.get_year()
+        """
+        Loads or updates date/time via the datetime_api.
+        """
+        if not self.datetime_api:
+            LOG.warning(f"Requested update before API's loaded!")
+            self._load_skill_apis()
+        self.gui["time_string"] = self.datetime_api.get_display_current_time()
+        self.gui["date_string"] = self.datetime_api.get_display_date()
+        self.gui["weekday_string"] = self.datetime_api.get_weekday()
+        self.gui['day_string'], self.gui["month_string"] = self._split_month_string(self.datetime_api.get_month_date())
+        self.gui["year_string"] = self.datetime_api.get_year()
 
     def update_weather(self):
+        """
+        Loads or updates weather via the weather_api.
+        """
         try:
             current_weather_report = self.weather_api.get_current_weather_homescreen()
             self.gui["weather_code"] = current_weather_report.get("weather_code")
             self.gui["weather_temp"] = current_weather_report.get("weather_temp")
-        except:
-            self.log.error("Failed To Fetch Weather Report")
-
-    def handle_mycroft_ready(self, message):
-        try:
-            if not self.weather_api:
-                self.weather_api = SkillApi.get('skill-weather.openvoiceos')
-        except:
-            self.log.error("Failed To Import Weather Skill")
-
-        try:
-            if not self.skill_info_api:
-                self.skill_info_api = SkillApi.get('ovos-skills-info.openvoiceos')
-        except:
-            self.log.error("Failed To Import OVOS Info Skill")
+        except Exception as e:
+            LOG.error(f"Failed To Fetch Weather Report: {e}")
 
     #####################################################################
     # Wallpaper Manager
 
     def collect_wallpapers(self):
+        def_wallpaper_collection, loc_wallpaper_collection = None, None
         for dirname, dirnames, filenames in os.walk(self.def_wallpaper_folder):
             def_wallpaper_collection = filenames
         
@@ -163,7 +139,7 @@ class OVOSHomescreen(MycroftSkill):
         self.wallpaper_collection = def_wallpaper_collection + loc_wallpaper_collection
 
     @intent_file_handler("change.wallpaper.intent")
-    def change_wallpaper(self, message):
+    def change_wallpaper(self, _):
         # Get Current Wallpaper idx
         current_idx = self.get_wallpaper_idx(self.selected_wallpaper)
         collection_length = len(self.wallpaper_collection) - 1
@@ -193,8 +169,7 @@ class OVOSHomescreen(MycroftSkill):
         if image_url:
             print(image_url)
             response = requests.get(image_url)
-            with self.file_system.open(
-                path.join("wallpapers", setname), "wb") as my_file:
+            with self.file_system.open(path.join("wallpapers", setname), "wb") as my_file:
                 my_file.write(response.content)
                 my_file.close()
             self.collect_wallpapers()
@@ -294,6 +269,61 @@ class OVOSHomescreen(MycroftSkill):
 
     def stop(self):
         pass
+
+    def shutdown(self):
+        self.cancel_all_repeating_events()
+
+    def handle_mycroft_ready(self, _):
+        self._load_skill_apis()
+
+    def _load_skill_apis(self):
+        """
+        Loads weather, date/time, and examples skill APIs
+        """
+        try:
+            if not self.weather_api:
+                self.weather_api = SkillApi.get(self.weather_skill)
+        except Exception as e:
+            LOG.error(f"Failed To Import Weather Skill: {e}")
+
+        try:
+            if not self.skill_info_api:
+                self.skill_info_api = SkillApi.get(self.skill_info_skill)
+        except Exception as e:
+            LOG.error(f"Failed To Import OVOS Info Skill: {e}")
+
+        # Import Date Time Skill As Date Time Provider
+        try:
+            self.datetime_api = SkillApi.get(self.datetime_skill)
+        except Exception as e:
+            # TODO: Depreciate this
+            LOG.error(f"Failed to import DateTime Skill: {e}")
+            try:
+                root_dir = self.root_dir.rsplit("/", 1)[0]
+                time_date_path = str(root_dir) + "/date-time.neon/__init__.py"
+                time_date_id = "datetimeskill"
+                datetimeskill = load_skill_module(time_date_path, time_date_id)
+                from datetimeskill import TimeSkill
+
+                self.datetime_api = TimeSkill()
+            except Exception as e:
+                LOG.error(f"Failed To Import DateTime Skill: {e}")
+
+    def _split_month_string(self, month_date: str) -> list:
+        """
+        Splits a month+date string into month and date (i.e. "August 06" -> ["August", "06"])
+        :param month_date: formatted month and day of month ("August 06" or "06 August")
+        :return: [day, month]
+        """
+        month_string = month_date.split(" ")
+        if self.config_core.get('date_format') == 'MDY':
+            day_string = month_string[1]
+            month_string = month_string[0]
+        else:
+            day_string = month_string[0]
+            month_string = month_string[1]
+
+        return [day_string, month_string]
 
 
 def create_skill():
