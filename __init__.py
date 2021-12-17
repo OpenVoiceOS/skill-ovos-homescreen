08b06288b01dbb9d534d6b9a1aba122940e28e95
Aditya Mehra <aix.m@outlook.com>
2021-12-17 20:24:46 +1030

    add support for various date formats for different regions

--- __init__.py
+++ __init__.py
@@ -86,6 +86,7 @@ class OVOSHomescreenSkill(MycroftSkill):
             LOG.error(e)
 
         self.gui['rtl_mode'] = self.rtlMode
+        self.gui['dateFormat'] = "DMY"
         self.gui.show_page("idle.qml")
 
     def update_examples(self):
