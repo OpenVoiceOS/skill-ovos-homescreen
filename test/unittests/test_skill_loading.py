1a3b36b3022a156c5b35ea47cf775964c5b3cfd5
jarbasai <jarbasai@mailfence.com>
2022-10-18 23:43:18 +0100

    automtions

--- test/unittests/test_skill_loading.py
+++ test/unittests/test_skill_loading.py
@@ -5,7 +5,7 @@ from ovos_utils.bracket_expansion import expand_parentheses, expand_options
 
 from adapt.engine import IntentDeterminationEngine
 from adapt.intent import IntentBuilder
-from skill_ovos_homescreen import VOSHomescreenSkill, create_skill
+from skill_ovos_homescreen import OVOSHomescreenSkill, create_skill
 from ovos_plugin_manager.skills import find_skill_plugins
 from ovos_utils.messagebus import FakeBus
 from mycroft.skills.skill_loader import PluginSkillLoader, SkillLoader
@@ -19,7 +19,7 @@ class TestSkillLoading(unittest.TestCase):
 
     def test_from_class(self):
         bus = FakeBus()
-        skill = VOSHomescreenSkill()
+        skill = OVOSHomescreenSkill()
         skill._startup(bus, self.skill_id)
         self.assertEqual(skill.bus, bus)
         self.assertEqual(skill.skill_id, self.skill_id)
